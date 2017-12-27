979010cd388c040bb6914490ad56529e915c6414
Florian <f.m.coulon@gmail.com>
2017-12-27 17:42:23 +0100

    tab !

--- React_Starter/src/views/Dashboard/Dashboard.js
+++ React_Starter/src/views/Dashboard/Dashboard.js
@@ -22,13 +22,37 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        <div>
-        <ul>
-          {this.state.results.map(result =>
-            <li>{result.id_flu}</li>
-          )}
-        </ul>
-      </div>
+        
+      <Table responsive striped>
+                  <thead>
+                  <tr>
+                    <th>Flux ID</th>
+                    <th>Type </th>
+                    <th>Emetteur</th>
+                    <th>Destinataire</th>
+                    <th>Reference</th>
+                    <th>Creation</th>
+                    <th>Dernier Traitement</th>
+                    <th>Statut</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {
+                  	this.state.results.map(result =>
+            			<tr>
+            				<td>{ result.id_flu }</td>
+            				<td>{ result.typ_flu }</td>
+            				<td>{ result.eme_flu }</td>
+            				<td>{ result.des_flu }</td>
+            				<td>{ result.ref_flu }</td>
+            				<td>{ result.ts_cre }</td>
+            				<td>{ "Requete pas encore implementée" }</td>
+            				<td><Badge color="success">{ result.sta_flu }</Badge></td>
+            			</tr>
+          			)
+                  }
+                  </tbody>
+                </Table>
       </div>
     )
   }

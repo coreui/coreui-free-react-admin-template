dff961358268066c9c2862bc22f55f7c76fd6b5e
Florian <f.m.coulon@gmail.com>
2017-12-27 17:05:35 +0100

    Test subbredit query

--- React_Starter/src/views/Dashboard/Dashboard.js
+++ React_Starter/src/views/Dashboard/Dashboard.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+  
+  componentDidMount() {
+   	axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
+   	  .then(res => {
+   	    const posts = res.data.data.children.map(obj => obj.data);
+   	    this.setState({ posts });
+   	  });
+  }
 
   render() {
     return (
       <div className="animated fadeIn">
-        Yolo World
+        <div>
+        <h1>{`/r/${this.props.subreddit}`}</h1>
+        <ul>
+          {this.state.posts.map(post =>
+            <li key={post.id}>{post.title}</li>
+          )}
+        </ul>
+      </div>
       </div>
     )
   }

baf0a726bbed64721a4355ffa0208de305e70396
Florian <f.m.coulon@gmail.com>
2018-01-02 11:21:10 +0100

    add form

--- React_Starter/src/views/Dashboard/Dashboard.js
+++ React_Starter/src/views/Dashboard/Dashboard.js
@@ -82,13 +82,80 @@ class Dashboard extends Component {
   		return(gln_flu)
   	}
   }
-
-
-
   
   render() {
     return (
       <div className="animated fadeIn">
+        <Col xs="12" sm="6">
+            <Card>
+              <CardHeader>
+                <strong>Credit Card</strong>
+                <small> Form</small>
+              </CardHeader>
+              <CardBody>
+                <Row>
+                  <Col xs="12">
+                    <FormGroup>
+                      <Label htmlFor="name">Name</Label>
+                      <Input type="text" id="name" placeholder="Enter your name" required/>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs="12">
+                    <FormGroup>
+                      <Label htmlFor="ccnumber">Credit Card Number</Label>
+                      <Input type="text" id="ccnumber" placeholder="0000 0000 0000 0000" required/>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs="4">
+                    <FormGroup>
+                      <Label htmlFor="ccmonth">Month</Label>
+                      <Input type="select" name="ccmonth" id="ccmonth">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                  <Col xs="4">
+                    <FormGroup>
+                      <Label htmlFor="ccyear">Year</Label>
+                      <Input type="select" name="ccyear" id="ccyear">
+                        <option>2017</option>
+                        <option>2018</option>
+                        <option>2019</option>
+                        <option>2020</option>
+                        <option>2021</option>
+                        <option>2022</option>
+                        <option>2023</option>
+                        <option>2024</option>
+                        <option>2025</option>
+                        <option>2026</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                  <Col xs="4">
+                    <FormGroup>
+                      <Label htmlFor="cvv">CVV/CVC</Label>
+                      <Input type="text" id="cvv" placeholder="123" required/>
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
         <Col xs="12" lg="12">
             <Card>
               <CardHeader>

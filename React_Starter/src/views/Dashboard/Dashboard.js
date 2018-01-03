f7e55ab2fb50e4036c0f9540755eea8c1ce0b1fa
Florian <f.m.coulon@gmail.com>
2018-01-03 09:37:59 +0100

    change month digits to text label

--- React_Starter/src/views/Dashboard/Dashboard.js
+++ React_Starter/src/views/Dashboard/Dashboard.js
@@ -151,18 +151,18 @@ class Dashboard extends Component {
                     <FormGroup>
                       <Label htmlFor="ccmonth">Month</Label>
                       <Input type="select" name="ccmonth" id="ccmonth">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
+                        <option value="1">Janvier</option>
+                        <option value="2">Février</option>
+                        <option value="3">Mars</option>
+                        <option value="4">Avril</option>
+                        <option value="5">Mai</option>
+                        <option value="6">Juin</option>
+                        <option value="7">Juillet</option>
+                        <option value="8">Aout</option>
+                        <option value="9">Septembre</option>
+                        <option value="10">Octobre</option>
+                        <option value="11">Novembre</option>
+                        <option value="12">Décembre</option>
                       </Input>
                     </FormGroup>
                   </Col>

b1ba7d8cd44e0f97a323fb0abceff07c2e31bb75
xidedix <xidedix@users.noreply.github.com>
2018-04-11 18:34:16 +0200

    refactor(FullAside): ListGroup (deprecate callout)

--- src/containers/Full/FullAside.js
+++ src/containers/Full/FullAside.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input, Label, Nav, NavItem, NavLink, Progress, TabContent, TabPane } from 'reactstrap';
+import { Input, Label, Nav, NavItem, NavLink, Progress, TabContent, TabPane, ListGroup, ListGroupItem } from 'reactstrap';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -30,6 +30,7 @@ class FullAside extends Component {
 
   render() {
 
+    // eslint-disable-next-line
     const { children, ...attributes } = this.props;
 
     return (
@@ -62,90 +63,89 @@ class FullAside extends Component {
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
           <TabPane tabId="1">
-            <div className="callout m-0 py-2 text-muted text-center bg-light text-uppercase">
-              <small><b>Today</b></small>
-            </div>
-            <hr className="transparent mx-3 my-0" />
-            <div className="callout callout-warning m-0 py-3">
-              <div className="avatar float-right">
-                <img src={'assets/img/avatars/7.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-              </div>
-              <div>Meeting with <strong>Lucas</strong></div>
-              <small className="text-muted mr-3"><i className="icon-calendar"></i>&nbsp; 1 - 3pm</small>
-              <small className="text-muted"><i className="icon-location-pin"></i>&nbsp; Palo Alto, CA</small>
-            </div>
-            <hr className="mx-3 my-0" />
-            <div className="callout callout-info m-0 py-3">
-              <div className="avatar float-right">
-                <img src={'assets/img/avatars/4.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-              </div>
-              <div>Skype with <strong>Megan</strong></div>
-              <small className="text-muted mr-3"><i className="icon-calendar"></i>&nbsp; 4 - 5pm</small>
-              <small className="text-muted"><i className="icon-social-skype"></i>&nbsp; On-line</small>
-            </div>
-            <hr className="transparent mx-3 my-0" />
-            <div className="callout m-0 py-2 text-muted text-center bg-light text-uppercase">
-              <small><b>Tomorrow</b></small>
-            </div>
-            <hr className="transparent mx-3 my-0" />
-            <div className="callout callout-danger m-0 py-3">
-              <div>New UI Project - <strong>deadline</strong></div>
-              <small className="text-muted mr-3"><i className="icon-calendar"></i>&nbsp; 10 - 11pm</small>
-              <small className="text-muted"><i className="icon-home"></i>&nbsp; creativeLabs HQ</small>
-              <div className="avatars-stack mt-2">
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/2.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+            <ListGroup className="list-group-accent">
+              <ListGroupItem className="list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase small">Today</ListGroupItem>
+              <ListGroupItem action tag="a" href="#" className="list-group-item-accent-warning list-group-item-divider">
+                <div className="avatar float-right">
+                  <img className="img-avatar" src="assets/img/avatars/7.jpg" alt="admin@bootstrapmaster.com"></img>
                 </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/3.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                <div>Meeting with <strong>Lucas</strong> </div>
+                <small className="text-muted mr-3">
+                  <i className="icon-calendar"></i>&nbsp; 1 - 3pm
+                </small>
+                <small className="text-muted">
+                  <i className="icon-location-pin"></i> Palo Alto, CA
+                </small>
+              </ListGroupItem>
+              <ListGroupItem action tag="a" href="#" className="list-group-item-accent-info list-group-item-divider">
+                <div className="avatar float-right">
+                  <img className="img-avatar" src="assets/img/avatars/4.jpg" alt="admin@bootstrapmaster.com"></img>
                 </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/4.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                <div>Skype with <strong>Megan</strong></div>
+                <small className="text-muted mr-3">
+                  <i className="icon-calendar"></i>&nbsp; 4 - 5pm
+                </small>
+                <small className="text-muted">
+                  <i className="icon-social-skype"></i> On-line
+                </small>
+              </ListGroupItem>
+              <ListGroupItem className="list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase small">Tomorrow</ListGroupItem>
+              <ListGroupItem action tag="a" href="#" className="list-group-item-accent-danger list-group-item-divider">
+                <div>New UI Project - <strong>deadline</strong></div>
+                <small className="text-muted mr-3"><i className="icon-calendar"></i>&nbsp; 10 - 11pm</small>
+                <small className="text-muted"><i className="icon-home"></i>&nbsp; creativeLabs HQ</small>
+                <div className="avatars-stack mt-2">
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/2.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/3.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/4.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/5.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
                 </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/5.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+              </ListGroupItem>
+              <ListGroupItem action tag="a" href="#" className="list-group-item-accent-success list-group-item-divider">
+                <div><strong>#10 Startups.Garden</strong> Meetup</div>
+                <small className="text-muted mr-3"><i className="icon-calendar"></i>&nbsp; 1 - 3pm</small>
+                <small className="text-muted"><i className="icon-location-pin"></i>&nbsp; Palo Alto, CA</small>
+              </ListGroupItem>
+              <ListGroupItem action tag="a" href="#" className="list-group-item-accent-primary list-group-item-divider">
+                <div><strong>Team meeting</strong></div>
+                <small className="text-muted mr-3"><i className="icon-calendar"></i>&nbsp; 4 - 6pm</small>
+                <small className="text-muted"><i className="icon-home"></i>&nbsp; creativeLabs HQ</small>
+                <div className="avatars-stack mt-2">
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/2.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/3.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/4.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/5.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/7.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
+                  <div className="avatar avatar-xs">
+                    <img src={'assets/img/avatars/8.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+                  </div>
                 </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-                </div>
-              </div>
-            </div>
-            <hr className="mx-3 my-0" />
-            <div className="callout callout-success m-0 py-3">
-              <div><strong>#10 Startups.Garden</strong> Meetup</div>
-              <small className="text-muted mr-3"><i className="icon-calendar"></i>&nbsp; 1 - 3pm</small>
-              <small className="text-muted"><i className="icon-location-pin"></i>&nbsp; Palo Alto, CA</small>
-            </div>
-            <hr className="mx-3 my-0" />
-            <div className="callout callout-primary m-0 py-3">
-              <div><strong>Team meeting</strong></div>
-              <small className="text-muted mr-3"><i className="icon-calendar"></i>&nbsp; 4 - 6pm</small>
-              <small className="text-muted"><i className="icon-home"></i>&nbsp; creativeLabs HQ</small>
-              <div className="avatars-stack mt-2">
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/2.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-                </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/3.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-                </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/4.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-                </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/5.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-                </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-                </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/7.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-                </div>
-                <div className="avatar avatar-xs">
-                  <img src={'assets/img/avatars/8.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-                </div>
-              </div>
-            </div>
-            <hr className="mx-3 my-0" />
+              </ListGroupItem>
+            </ListGroup>
           </TabPane>
           <TabPane tabId="2" className="p-3">
             <div className="message">

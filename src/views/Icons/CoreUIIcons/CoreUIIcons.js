96d21c95e3d83a0b2041b02fff69fb7c60825aed
xidedix <xidedix@users.noreply.github.com>
2018-05-23 12:43:43 +0200

    refactor: CoreUI Icons v0.2.0

--- src/views/Icons/CoreUIIcons/CoreUIIcons.js
+++ src/views/Icons/CoreUIIcons/CoreUIIcons.js
@@ -17,6 +17,242 @@ class CoreUIIcons extends Component {
           <CardBody>
             <Row className="text-center">
               <Col xs="6" sm="4" md="3">
+                <i className="cui-account-logout icons font-2xl d-block mt-4"></i>
+                <div>account-logout</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-action-redo icons font-2xl d-block mt-4"></i>
+                <div>action-redo</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-action-undo icons font-2xl d-block mt-4"></i>
+                <div>action-undo</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-align-center icons font-2xl d-block mt-4"></i>
+                <div>align-center</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-align-left icons font-2xl d-block mt-4"></i>
+                <div>align-left</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-align-right icons font-2xl d-block mt-4"></i>
+                <div>align-right</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-aperture icons font-2xl d-block mt-4"></i>
+                <div>aperture</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-bottom icons font-2xl d-block mt-4"></i>
+                <div>arrow-bottom</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-circle-bottom icons font-2xl d-block mt-4"></i>
+                <div>arrow-circle-bottom</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-circle-left icons font-2xl d-block mt-4"></i>
+                <div>arrow-circle-left</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-circle-right icons font-2xl d-block mt-4"></i>
+                <div>arrow-circle-right</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-circle-top icons font-2xl d-block mt-4"></i>
+                <div>arrow-circle-top</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-left icons font-2xl d-block mt-4"></i>
+                <div>arrow-left</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-right icons font-2xl d-block mt-4"></i>
+                <div>arrow-right</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-thick-bottom icons font-2xl d-block mt-4"></i>
+                <div>arrow-thick-bottom</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-thick-left icons font-2xl d-block mt-4"></i>
+                <div>arrow-thick-left</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-thick-right icons font-2xl d-block mt-4"></i>
+                <div>arrow-thick-right</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-thick-top icons font-2xl d-block mt-4"></i>
+                <div>arrow-thick-top</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-arrow-top icons font-2xl d-block mt-4"></i>
+                <div>arrow-top</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-audio icons font-2xl d-block mt-4"></i>
+                <div>audio</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-audio-spectrum icons font-2xl d-block mt-4"></i>
+                <div>audio-spectrum</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-badge icons font-2xl d-block mt-4"></i>
+                <div>badge</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-ban icons font-2xl d-block mt-4"></i>
+                <div>ban</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-bar-chart icons font-2xl d-block mt-4"></i>
+                <div>bar-chart</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-battery-empty icons font-2xl d-block mt-4"></i>
+                <div>battery-empty</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-battery-full icons font-2xl d-block mt-4"></i>
+                <div>battery-full</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-beaker icons font-2xl d-block mt-4"></i>
+                <div>beaker</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-bluetooth icons font-2xl d-block mt-4"></i>
+                <div>bluetooth</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-bold icons font-2xl d-block mt-4"></i>
+                <div>bold</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-bolt icons font-2xl d-block mt-4"></i>
+                <div>bolt</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-book icons font-2xl d-block mt-4"></i>
+                <div>book</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-bookmark icons font-2xl d-block mt-4"></i>
+                <div>bookmark</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-box icons font-2xl d-block mt-4"></i>
+                <div>box</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-briefcase icons font-2xl d-block mt-4"></i>
+                <div>briefcase</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-british-pound icons font-2xl d-block mt-4"></i>
+                <div>british-pound</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-browser icons font-2xl d-block mt-4"></i>
+                <div>browser</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-brush icons font-2xl d-block mt-4"></i>
+                <div>brush</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-bug icons font-2xl d-block mt-4"></i>
+                <div>bug</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-bullhorn icons font-2xl d-block mt-4"></i>
+                <div>bullhorn</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-caret-bottom icons font-2xl d-block mt-4"></i>
+                <div>caret-bottom</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-caret-left icons font-2xl d-block mt-4"></i>
+                <div>caret-left</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-caret-right icons font-2xl d-block mt-4"></i>
+                <div>caret-right</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-caret-top icons font-2xl d-block mt-4"></i>
+                <div>caret-top</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-cart icons font-2xl d-block mt-4"></i>
+                <div>cart</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-chat icons font-2xl d-block mt-4"></i>
+                <div>chat</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-check icons font-2xl d-block mt-4"></i>
+                <div>check</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-chevron-bottom icons font-2xl d-block mt-4"></i>
+                <div>chevron-bottom</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-chevron-left icons font-2xl d-block mt-4"></i>
+                <div>chevron-left</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-chevron-right icons font-2xl d-block mt-4"></i>
+                <div>chevron-right</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-chevron-top icons font-2xl d-block mt-4"></i>
+                <div>chevron-top</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-circle-check icons font-2xl d-block mt-4"></i>
+                <div>circle-check</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-circle-x icons font-2xl d-block mt-4"></i>
+                <div>circle-x</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-clipboard icons font-2xl d-block mt-4"></i>
+                <div>clipboard</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-clock icons font-2xl d-block mt-4"></i>
+                <div>clock</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-cloud icons font-2xl d-block mt-4"></i>
+                <div>cloud</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-cloud-upload icons font-2xl d-block mt-4"></i>
+                <div>cloud-upload</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-cloudy icons font-2xl d-block mt-4"></i>
+                <div>cloudy</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-social-linkedin icons font-2xl d-block mt-4"></i>
+                <div>social-linkedin</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
+                <i className="cui-social-spotify icons font-2xl d-block mt-4"></i>
+                <div>social-spotify</div>
+              </Col>
+              <Col xs="6" sm="4" md="3">
                 <i className="cui-basket-loaded icons font-2xl d-block mt-4"></i>
                 <div>basket-loaded</div>
               </Col>
@@ -181,3 +417,4 @@ class CoreUIIcons extends Component {
 }
 
 export default CoreUIIcons;
+

66d3f04d92d410b0eb9a049b9fbff019d0682b6b
jrousseau <v4lproik@gmail.com>
2019-01-14 15:50:40 +0100

    Fix url collection

--- bin/generate_config.sh
+++ bin/generate_config.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -eu
-if [ -z "${API_URL:-}" ]; then
-API_URL_JSON=undefined
-else
-API_URL_JSON=$(jq -n --arg api_url '$API_URL' '$api_url')
-fi
+API_URL_JSON=$(echo $REACT_APP_API_URL)
 
 cat <<EOF
-window.REACT_APP_API_URL=$API_URL_JSON;
+window.REACT_APP_API_URL="$API_URL_JSON";
 EOF

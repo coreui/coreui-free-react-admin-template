7c208477bb9ce6b47701c7d79cbcb73c19c0a220
jrousseau <v4lproik@gmail.com>
2019-01-19 07:40:52 +0100

    Add minimized version of logo

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
 import logo from '../../assets/img/brand/Damper-logo-2019-v4.png'
-import sygnet from '../../assets/img/brand/sygnet.svg'
+import sygnet from '../../assets/img/brand/logo-damper-no-title.png'
 
 const propTypes = {
   children: PropTypes.node,
@@ -25,8 +25,8 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 140, height: 35, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+          full={{ src: logo, width: 140, height: 35, alt: 'Damper Logo' }}
+          minimized={{ src: sygnet, width: 40, height: 40, alt: 'Damper Logo' }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
 

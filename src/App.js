ded4437ae10cc03eb75c5eca89c266c3ce2dc3a5
xidedix <xidedix@users.noreply.github.com>
2019-03-25 14:27:54 +0100

    refactor(App): change to render in Route

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ class App extends Component {
       <HashRouter>
           <React.Suspense fallback={loading()}>
             <Switch>
-              <Route exact path="/login" name="Login Page" component={Login} />
-              <Route exact path="/register" name="Register Page" component={Register} />
-              <Route exact path="/404" name="Page 404" component={Page404} />
-              <Route exact path="/500" name="Page 500" component={Page500} />
-              <Route path="/" name="Home" component={DefaultLayout} />
+              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
+              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+              <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
             </Switch>
           </React.Suspense>
       </HashRouter>

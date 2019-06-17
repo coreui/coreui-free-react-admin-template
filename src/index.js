969bc8593af27c55058cade46adb71469eb93c7a
xidedix <xidedix@users.noreply.github.com>
2019-06-17 20:52:55 +0200

    refactor: add ie polyfills

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'react-app-polyfill/ie9'; // For IE 9-11 support
-import 'react-app-polyfill/ie11'; // For IE 11 support
+import 'react-app-polyfill/stable';
+// import 'react-app-polyfill/ie11'; // For IE 11 support
 import './polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';

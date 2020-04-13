0c088dbca0931da0b109e2fd4221d72ee9a099e7
Xiaozhe Wang <xiaozhewang@Xiaozhes-MacBook-Pro.local>
2020-04-13 02:01:43 -0700

    [RedBlock][Register][3]Remove Facebook and Twitter footer

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -3,7 +3,6 @@ import {
   Button,
   Card,
   CardBody,
-  CardFooter,
   Col,
   Container,
   Form,
@@ -112,20 +111,6 @@ class Register extends Component {
                     </Button>
                   </Form>
                 </CardBody>
-                <CardFooter className="p-4">
-                  <Row>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-facebook mb-1" block>
-                        <span>facebook</span>
-                      </Button>
-                    </Col>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-twitter mb-1" block>
-                        <span>twitter</span>
-                      </Button>
-                    </Col>
-                  </Row>
-                </CardFooter>
               </Card>
             </Col>
           </Row>

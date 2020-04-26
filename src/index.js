c3a164d7ef8446a7efa136568e8ce98d19bdd23b
Xiaozhe Wang <xiaozhewang@Xiaozhes-MacBook-Pro.local>
2020-04-26 01:09:47 -0700

    [RedBlock][Register][6]Set up Apollo client for the app

--- src/index.js
+++ src/index.js
@@ -7,8 +7,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'http://localhost/graphql',
+});
+
+const AppContainer = () => (
+    <ApolloProvider client={client} >
+        <App />
+    </ApolloProvider>
+)
+
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

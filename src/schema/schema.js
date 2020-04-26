f42a74250c8554e9a935f7b2412e2a821e994a7c
Xiaozhe Wang <xiaozhewang@Xiaozhes-MacBook-Pro.local>
2020-04-26 01:13:48 -0700

    [RedBlock][Register][7]Add graphql mutation

---
+++ src/schema/schema.js
@@ -0,0 +1,31 @@
+import { gql } from "apollo-boost";
+
+export const SIGNIN_USER = gql`
+  mutation($userName: String!, $password: String!) {
+    signinUser(userName: $userName, password: $password) {
+      token
+    }
+  }
+`;
+
+export const SIGNUP_USER = gql`
+  mutation(
+    $userName: String!
+    $password: String!
+    $email: String!
+    $companyName: String!
+    $firstName: String!
+    $lastName: String!
+  ) {
+    signupUser(
+      userName: "$userName"
+      password: "$password"
+      email: "$email"
+      companyName: "$companyName"
+      firstName: "$firstName"
+      lastName: "$lastName"
+    ) {
+      token
+    }
+  }
+`;

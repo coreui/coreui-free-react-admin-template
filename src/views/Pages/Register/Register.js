0d0aebd5b95c1db52a0eb6c2ee0826c54da4199a
Xiaozhe Wang <xiaozhewang@Xiaozhes-MacBook-Pro.local>
2020-04-26 01:18:31 -0700

    [RedBlock][Register][8]Add graphql mutation and submit button handler to trigger this register mutation

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -12,8 +12,17 @@ import {
   InputGroupText,
   Row,
 } from "reactstrap";
+import { SIGNUP_USER } from "../../../schema/schema";
+import { Mutation } from "react-apollo";
 
 class Register extends Component {
+  handleSubmit(event, signupUser) {
+    event.preventDefault();
+    signupUser()
+      .then(async ({ data }) => {})
+      .catch(() => {});
+  }
+
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -22,94 +31,105 @@ class Register extends Component {
             <Col md="9" lg="7" xl="6">
               <Card className="mx-4">
                 <CardBody className="p-4">
-                  <Form>
-                    <h1>Register</h1>
-                    <p className="text-muted">Create your account</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="text"
-                        placeholder="Username"
-                        autoComplete="username"
-                      />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="new-password"
-                      />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="password"
-                        placeholder="Repeat password"
-                        autoComplete="new-password"
-                      />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>@</InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="text"
-                        placeholder="Company Email"
-                        autoComplete="company-email"
-                      />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-people"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="company-name"
-                        placeholder="Company Name"
-                        autoComplete="company-name"
-                      />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Row>
-                        <Col xs="12" sm="6">
-                          <Input
-                            type="first-name"
-                            placeholder="First Name"
-                            autoComplete="first-name"
-                          />
-                        </Col>
-                        <Col xs="12" sm="6">
-                          <Input
-                            type="last-name"
-                            placeholder="Last Name"
-                            autoComplete="last-name"
-                          />
-                        </Col>
-                      </Row>
-                    </InputGroup>
-                    <Button color="success" block>
-                      Create Account
-                    </Button>
-                  </Form>
+                  <Mutation mutation={SIGNUP_USER}>
+                    {(signupUser, { loading }) => {
+                      return (
+                        <Form>
+                          <h1>Register</h1>
+                          <p className="text-muted">Create your account</p>
+                          <InputGroup className="mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="icon-user"></i>
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              type="text"
+                              placeholder="Username"
+                              autoComplete="username"
+                            />
+                          </InputGroup>
+                          <InputGroup className="mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="icon-lock"></i>
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              type="password"
+                              placeholder="Password"
+                              autoComplete="new-password"
+                            />
+                          </InputGroup>
+                          <InputGroup className="mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="icon-lock"></i>
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              type="password"
+                              placeholder="Repeat password"
+                              autoComplete="new-password"
+                            />
+                          </InputGroup>
+                          <InputGroup className="mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>@</InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              type="text"
+                              placeholder="Company Email"
+                              autoComplete="company-email"
+                            />
+                          </InputGroup>
+                          <InputGroup className="mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="icon-people"></i>
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              type="company-name"
+                              placeholder="Company Name"
+                              autoComplete="company-name"
+                            />
+                          </InputGroup>
+                          <InputGroup className="mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="icon-user"></i>
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Row>
+                              <Col xs="12" sm="6">
+                                <Input
+                                  type="first-name"
+                                  placeholder="First Name"
+                                  autoComplete="first-name"
+                                />
+                              </Col>
+                              <Col xs="12" sm="6">
+                                <Input
+                                  type="last-name"
+                                  placeholder="Last Name"
+                                  autoComplete="last-name"
+                                />
+                              </Col>
+                            </Row>
+                          </InputGroup>
+                          <Button
+                            color="success"
+                            onClick={(event) =>
+                              this.handleSubmit(event, signupUser)
+                            }
+                          >
+                            Create Account
+                          </Button>
+                        </Form>
+                      );
+                    }}
+                  </Mutation>
                 </CardBody>
               </Card>
             </Col>

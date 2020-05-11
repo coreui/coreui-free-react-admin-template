e1aa2c1f02ba96b11d478988ba2f5f1b3da40003
Xiaozhe Wang <xiaozhewang@Xiaozhes-MacBook-Pro.local>
2020-05-11 00:50:34 -0700

    [RedBlock][Register][24]Use the correct uri

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import 'react-app-polyfill/ie9'; // For IE 9-11 support
-import 'react-app-polyfill/stable';
+import "react-app-polyfill/ie9"; // For IE 9-11 support
+import "react-app-polyfill/stable";
 // import 'react-app-polyfill/ie11'; // For IE 11 support
-import './polyfill'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-    uri: 'http://localhost/graphql',
+  uri: "http://47.89.249.216/graphql",
 });
 
 const AppContainer = () => (
-    <ApolloProvider client={client} >
-        <App />
-    </ApolloProvider>
-)
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
 
-ReactDOM.render(<AppContainer />, document.getElementById('root'));
-
-
+ReactDOM.render(<AppContainer />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

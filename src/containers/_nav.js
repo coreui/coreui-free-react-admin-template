be5d1f0618f981f18c45be87afb56c4409bd3389
woothu <32914662+woothu@users.noreply.github.com>
2020-07-30 12:37:29 +0200

    refactor: add CIcon example in _nav.js

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -1,9 +1,12 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+
 export default [
   {
     _tag: 'CSidebarNavItem',
     name: 'Dashboard',
     to: '/dashboard',
-    icon: 'cil-speedometer',
+    icon: <CIcon name="cil-speedometer" customClasses="c-sidebar-nav-icon"/>,
     badge: {
       color: 'info',
       text: 'NEW',

f64c134cfa58b789b46949a384ebc4dcbe986efa
EricV <d175sfy@costco.com>
2020-08-09 09:40:37 -0400

    added badge to store div

--- src/views/dispatch/stores/createstore.js
+++ src/views/dispatch/stores/createstore.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Stores from '../../../utils/stores/storelist.js'
+import Badge from '../badge/badge.js'
 import shortid from 'shortid'
 import './createstore.css'
 let stores= [...Stores]
@@ -12,7 +13,9 @@ class CreateStore extends Component{
 render(){
     let store = this.state.store
    return <div className="store-div" id="draggable">
-       {stores.map(store => <div class="store-div__content" draggable="true"><span>{store.storeID}</span></div>)}
+       
+       {stores.map(store => <div class="store-div__content" draggable="true"><span>{store.storeID}</span><Badge/><Badge /><Badge color="warning"/></div>)}
+  
    </div>
 }
 }

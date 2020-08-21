684e6728f8ceafe5bcdd981bceb0703c558e2e64
Eric Velez <d175sfy@costco.com>
2020-08-21 10:34:14 -0400

    redo createstore

--- src/views/dispatch/stores/createstore.js
+++ src/views/dispatch/stores/createstore.js
@@ -1,76 +1,21 @@
-import React, { Component,useRef, Fragment } from 'react'
-import Stores from '../../../utils/stores/storelist.js'
-import { useDrag, UseDrop } from 'react-dnd'
+import React, { useState } from 'react'
+import StoreList from '../../../utils/stores/storelist.js'
+import { useDrag } from 'react-dnd'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import './createstore.css'
-let stores= [...Stores]
+
+let storeList = [...StoreList]
+
+const CreateStore = () => {
+    const [storeList, setStoreList] = useState(storeList);
 
 
-const createStore = ({ item, index,moveItem, status}) => {
-        const ref = useRef(null);
-        const [, drop] = useDrop({
-            accept: ITEM_TYPE,
-            hover(item, monitor) {
-                if(!ref.current) {
-                    return;
-                }
-                const dragIndex = item.index;
-                const hoverIndex = index;
-
-                if(dragIndex === hoverIndex) {
-                    return;
-                }
-
-                const hoveredRect = ref.current.getBoundClientRect();
-                const hoverMiddleY = (hoverRect.bottom - hoverRect.top) / 2;
-                const mousePosition = monitor.getClientOffset();
-                const hoverClientY = mousePosition.y - hoverRect.top;
-
-                if(dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-                    return;
-                }
-
-                if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-                    return;
-                }
-
-                moveItem(dragIndex, HoverIndex);
-                item.index = hoverIndex;
-            }
-        })
-
-        const [{ isDragging }, drag] = useDrag({
-            item: { type: ITEM_TYPE, ...item, index },
-            collect: monitor =>({
-                isDragging: monitor.isDragging()
-            })
-        })
-        const [show, setShow] = useState(false);
-        const onOpen = () => setShow(true);
-        const onClose = ()=> setShow(false);
-
-        drag(drop(ref));
-
-        return (
-            <DndProvider>
-            <Fragment>
-                <div 
-                ref={ref}
-                style={{ opacity: isDragging ? 0: 1 }}
-                className= {"item"}
-                onClick={onOpen}
-                    >
-                    <div classNmae={"color-bar"} style={{ backgroundcolor: status.color}}></div>
-                    <p className={"item-title"}>{ item.content }</p>
-                    <p className={"item-status"}>{ item.icon }</p>
-                </div>
-                <window 
-                    item= {item}
-                    onClose={onClose}
-                    show={show}
-                />
-            </Fragment>
-            </DndProvider>
-        )
+    return <div>
+            {storeList.map((store) =>(
+                <div>store.storeID</div>))}
+        </div>
 }
+
+
+export default CreateStore

766c3e2638694f43d9f5c24a465736c7146644da
Eric Velez <d175sfy@costco.com>
2020-08-22 13:46:53 -0400

    no changes

--- src/views/dispatch/drivers/createdriver.js
+++ src/views/dispatch/drivers/createdriver.js
@@ -1,245 +1,205 @@
 import React, { Component } from 'react'
 import Drivers from '../../../utils/drivers/driverlist'
-import Shifts from '../../../utils/shifts/shiftList'
 import './createdriver.css'
 
-const CreateDriver = ()=> (
+const CreateDriver = () => (
     <div className="driver-container">
-<React.Fragment>
-<div className="driver-container-label"><span>8am</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="8am")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>9am</span><div className="expand-icon-div" onClick={expandDriverContent}><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="9am")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>11am</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="11am")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>12pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="12pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>2pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="2pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>3pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="3pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>4pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="4pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>5pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="5pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>6pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="6pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>7pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="7pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>730pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="730pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>830pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="830pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>930pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="930pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>11pm</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="11pm")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>015am</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="015am")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>230am</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="230am")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-    <div className="driver-container">
-<div className="driver-container-label"><span>3am</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="3am")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>330am</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="330am")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-<div className="driver-container-label"><span>5am</span><div className="expand-icon-div"><span class="material-icons">
-expand_more
-</span></div>
-    {Drivers.filter(driver => driver.shift ==="5am")
-        .map(driver => (
-           
-        <div className="driver-container-content">
-            {driver.name} ({driver.tractor})
-        </div>
-    
-    ))}
-    </div>
-    </div>
+        <React.Fragment>
+            <div className="driver-container-label"><h3>8am</h3>
+                {Drivers.filter(driver => driver.shift === "8am")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            
+            <div className="driver-container-label"><h3>9am</h3>
+                {Drivers.filter(driver => driver.shift === "9am")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>11am</h3>
+                {Drivers.filter(driver => driver.shift === "11am")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>12pm</h3>
+                {Drivers.filter(driver => driver.shift === "12pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>2pm</h3>
+                {Drivers.filter(driver => driver.shift === "2pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>3pm</h3>
+                {Drivers.filter(driver => driver.shift === "3pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>4pm</h3>
+                {Drivers.filter(driver => driver.shift === "4pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>5pm</h3>
+                {Drivers.filter(driver => driver.shift === "5pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>6pm</h3>
+                {Drivers.filter(driver => driver.shift === "6pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>7pm</h3>
+                {Drivers.filter(driver => driver.shift === "7pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>730pm</h3>
+                {Drivers.filter(driver => driver.shift === "730pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>830pm</h3>
+                {Drivers.filter(driver => driver.shift === "830pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>930pm</h3>
+                {Drivers.filter(driver => driver.shift === "930pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>11pm</h3>
+                {Drivers.filter(driver => driver.shift === "11pm")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>015am</h3>
+                {Drivers.filter(driver => driver.shift === "015am")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>230am</h3>
+                {Drivers.filter(driver => driver.shift === "230am")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>3am</h3>
+                {Drivers.filter(driver => driver.shift === "3am")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>330am</h3>
+                {Drivers.filter(driver => driver.shift === "330am")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
+            <div className="driver-container-label"><h3>5am</h3>
+                {Drivers.filter(driver => driver.shift === "5am")
+                    .map(driver => (
+
+                        <div className="driver-container-content">
+                            {driver.name} ({driver.tractor})
+                        </div>
+
+                    ))}
+            </div>
     </React.Fragment>
-    </div>
+</div>
 )
 
-    
+
 
 export default CreateDriver

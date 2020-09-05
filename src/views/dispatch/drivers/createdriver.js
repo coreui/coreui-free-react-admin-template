3fa3433b0bf7bb85c0994e549a7a13986af83ca1
Eric Velez <d175sfy@costco.com>
2020-09-05 10:01:25 -0400

    added sort down svg

--- src/views/dispatch/drivers/createdriver.js
+++ src/views/dispatch/drivers/createdriver.js
@@ -3,6 +3,7 @@ import { FaSortDown } from 'react-icons/fa';
 import Drivers from '../../../utils/drivers/driverlist'
 import { ItemTypes } from '../../../utils/DnD/items.js'
 import './createdriver.css'
+
 import { useDrop } from 'react-dnd';
 
 const CreateDriver = props => {
@@ -17,9 +18,9 @@ const CreateDriver = props => {
         });
 
     return(
-    <div className="driver-container">
+    <div className="driver-accordion">
         <React.Fragment>
-            <div className="driver-container-label" key={Drivers.id}><h3>8am</h3>
+            <div className="driver-accordion__label" key={Drivers.id}><h3>8am</h3>
                 {Drivers.filter(driver => driver.shift === "8am")
                     .map(driver => (
 
@@ -27,211 +28,218 @@ const CreateDriver = props => {
                             ref={drop} 
                             key={driver.id}
                             opacity={isOver ? alert('green') : 'red'}
-                            className="driver-container-content">
+                            className="driver-accordion__content">
                        
                             {driver.name} ({driver.tractor})
                             <input 
-                                className="switch-checkbox" 
+                                className="driver-accordion__input" 
                                 id="chkbx"
-                                type="checkbox"/>
-                               <label className="switch-label" htmlFor="chkbx">&#x2193; </label> 
-                        </div>
+                                type="checkbox"
+                                name="driver-accordion"
+                                />
+                                <label htmlFor="chkbx" className="driver-accordion__label">Load #1</label>
+                                    <div className="driver-accordion__loads">
+                                        <p>
+                                            Load #1  
+                                        </p>
+                                    </div>
+                            </div>
 
                     ))}
             </div>
             
-            <div className="driver-container-label"><h3>9am</h3>
+            <div className="driver-accordion__label"><h3>9am</h3>
                 {Drivers.filter(driver => driver.shift === "9am")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>11am</h3>
+            <div className="driver-accordion__label"><h3>11am</h3>
                 {Drivers.filter(driver => driver.shift === "11am")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>12pm</h3>
+            <div className="driver-accordion__label"><h3>12pm</h3>
                 {Drivers.filter(driver => driver.shift === "12pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>2pm</h3>
+            <div className="driver-accordion__label"><h3>2pm</h3>
                 {Drivers.filter(driver => driver.shift === "2pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>3pm</h3>
+            <div className="driver-accordion__label"><h3>3pm</h3>
                 {Drivers.filter(driver => driver.shift === "3pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>4pm</h3>
+            <div className="driver-accordion__label"><h3>4pm</h3>
                 {Drivers.filter(driver => driver.shift === "4pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>5pm</h3>
+            <div className="driver-accordion__label"><h3>5pm</h3>
                 {Drivers.filter(driver => driver.shift === "5pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>6pm</h3>
+            <div className="driver-accordion__label"><h3>6pm</h3>
                 {Drivers.filter(driver => driver.shift === "6pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>7pm</h3>
+            <div className="driver-accordion__label"><h3>7pm</h3>
                 {Drivers.filter(driver => driver.shift === "7pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>730pm</h3>
+            <div className="driver-accordion__label"><h3>730pm</h3>
                 {Drivers.filter(driver => driver.shift === "730pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>830pm</h3>
+            <div className="driver-accordion__label"><h3>830pm</h3>
                 {Drivers.filter(driver => driver.shift === "830pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>930pm</h3>
+            <div className="driver-accordion__label"><h3>930pm</h3>
                 {Drivers.filter(driver => driver.shift === "930pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>11pm</h3>
+            <div className="driver-accordion__label"><h3>11pm</h3>
                 {Drivers.filter(driver => driver.shift === "11pm")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>015am</h3>
+            <div className="driver-accordion__label"><h3>015am</h3>
                 {Drivers.filter(driver => driver.shift === "015am")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>230am</h3>
+            <div className="driver-accordion__label"><h3>230am</h3>
                 {Drivers.filter(driver => driver.shift === "230am")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>3am</h3>
+            <div className="driver-accordion__label"><h3>3am</h3>
                 {Drivers.filter(driver => driver.shift === "3am")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>330am</h3>
+            <div className="driver-accordion__label"><h3>330am</h3>
                 {Drivers.filter(driver => driver.shift === "330am")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>
 
                     ))}
             </div>
-            <div className="driver-container-label"><h3>5am</h3>
+            <div className="driver-accordion__label"><h3>5am</h3>
                 {Drivers.filter(driver => driver.shift === "5am")
                     .map(driver => (
 
-                        <div className="driver-container-content">
+                        <div className="driver-accordion__content">
                             {driver.name} ({driver.tractor})
                             <FaSortDown className="sortDown"/>
                         </div>

f3794129805e7711fd72800e5111a466f19133e2
Eric Velez <d175sfy@costco.com>
2020-09-10 08:53:00 -0400

    added whseCount to dispatch page

---
+++ src/utils/WhseCount.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import As400 from './As400'
+import './WhseCount.css'
+
+const Whse_222 = As400.filter(load => load.Warehouse === "222").length
+const Whse_226 = As400.filter(load => load.Warehouse === "226").length
+const Whse_230 = As400.filter(load => load.Warehouse === "230").length
+const Whse_237 = As400.filter(load => load.Warehouse === "237").length
+const Whse_240 = As400.filter(load => load.Warehouse === "240").length
+const Whse_241 = As400.filter(load => load.Warehouse === "241").length
+const Whse_243 = As400.filter(load => load.Warehouse === "243").length
+const Whse_244 = As400.filter(load => load.Warehouse === "244").length
+const Whse_245 = As400.filter(load => load.Warehouse === "245").length
+const Whse_246 = As400.filter(load => load.Warehouse === "246").length
+const Whse_248 = As400.filter(load => load.Warehouse === "248").length
+const Whse_305 = As400.filter(load => load.Warehouse === "305").length
+const Whse_306 = As400.filter(load => load.Warehouse === "306").length
+const Whse_310 = As400.filter(load => load.Warehouse === "310").length
+const Whse_315 = As400.filter(load => load.Warehouse === "315").length
+const Whse_316 = As400.filter(load => load.Warehouse === "316").length
+const Whse_318 = As400.filter(load => load.Warehouse === "318").length
+const Whse_320 = As400.filter(load => load.Warehouse === "320").length
+const Whse_321 = As400.filter(load => load.Warehouse === "321").length
+const Whse_322 = As400.filter(load => load.Warehouse === "322").length
+const Whse_323 = As400.filter(load => load.Warehouse === "323").length
+const Whse_324 = As400.filter(load => load.Warehouse === "324").length
+const Whse_326 = As400.filter(load => load.Warehouse === "326").length
+const Whse_329 = As400.filter(load => load.Warehouse === "329").length
+const Whse_729 = As400.filter(load => load.Warehouse === "729").length
+const Whse_739 = As400.filter(load => load.Warehouse === "739").length
+const Whse_749 = As400.filter(load => load.Warehouse === "749").length
+const Whse_1025 = As400.filter(load => load.Warehouse === "1025").length
+const Whse_1062 = As400.filter(load => load.Warehouse === "1062").length
+const Whse_1070 = As400.filter(load => load.Warehouse === "1070").length
+const Whse_1081 = As400.filter(load => load.Warehouse === "1081").length
+const Whse_1093 = As400.filter(load => load.Warehouse === "1093").length
+const Whse_1103 = As400.filter(load => load.Warehouse === "1103").length
+const Whse_1114 = As400.filter(load => load.Warehouse === "1114").length
+const Whse_1166 = As400.filter(load => load.Warehouse === "1166").length
+const Whse_1174 = As400.filter(load => load.Warehouse === "1174").length
+const Whse_1177 = As400.filter(load => load.Warehouse === "1177").length
+const Whse_1199 = As400.filter(load => load.Warehouse === "1199").length
+const Whse_1211 = As400.filter(load => load.Warehouse === "1211").length
+const Whse_1214 = As400.filter(load => load.Warehouse === "1214").length
+const Whse_1215 = As400.filter(load => load.Warehouse === "1215").length
+const Whse_1236 = As400.filter(load => load.Warehouse === "1236").length
+const Whse_1334 = As400.filter(load => load.Warehouse === "1334").length
+const Whse_1415 = As400.filter(load => load.Warehouse === "1415").length
+
+
+
+const WhseCount = () => {
+
+    return (
+
+        <React.Fragment>
+        <div className="whse-container">
+        <div className="whse-item__container"><div className="whse-item">222</div><div className="whse-count">{Whse_222}</div></div>
+        <div className="whse-item__container"><div className="whse-item">226</div><div className="whse-count">{Whse_226}</div></div>
+        <div className="whse-item__container"><div className="whse-item">230</div><div className="whse-count">{Whse_230}</div></div>
+        <div className="whse-item__container"><div className="whse-item">237</div><div className="whse-count">{Whse_237}</div></div>
+        <div className="whse-item__container"><div className="whse-item">240</div><div className="whse-count">{Whse_240}</div></div>
+        <div className="whse-item__container"><div className="whse-item">241</div><div className="whse-count">{Whse_241}</div></div>
+        <div className="whse-item__container"><div className="whse-item">243</div><div className="whse-count">{Whse_243}</div></div>
+        <div className="whse-item__container"><div className="whse-item">244</div><div className="whse-count">{Whse_244}</div></div>
+        <div className="whse-item__container"><div className="whse-item">245</div><div className="whse-count">{Whse_245}</div></div>
+        <div className="whse-item__container"><div className="whse-item">246</div><div className="whse-count">{Whse_246}</div></div>
+        <div className="whse-item__container"><div className="whse-item">248</div><div className="whse-count">{Whse_248}</div></div>
+        <div className="whse-item__container"><div className="whse-item">305</div><div className="whse-count">{Whse_305}</div></div>
+        <div className="whse-item__container"><div className="whse-item">306</div><div className="whse-count">{Whse_306}</div></div>
+        <div className="whse-item__container"><div className="whse-item">310</div><div className="whse-count">{Whse_310}</div></div>
+        <div className="whse-item__container"><div className="whse-item">315</div><div className="whse-count">{Whse_315}</div></div>
+        <div className="whse-item__container"><div className="whse-item">316</div><div className="whse-count">{Whse_316}</div></div>
+        <div className="whse-item__container"><div className="whse-item">318</div><div className="whse-count">{Whse_318}</div></div>
+        <div className="whse-item__container"><div className="whse-item">320</div><div className="whse-count">{Whse_320}</div></div>
+        <div className="whse-item__container"><div className="whse-item">321</div><div className="whse-count">{Whse_321}</div></div>
+        <div className="whse-item__container"><div className="whse-item">322</div><div className="whse-count">{Whse_322}</div></div>
+        <div className="whse-item__container"><div className="whse-item">323</div><div className="whse-count">{Whse_323}</div></div>
+        <div className="whse-item__container"><div className="whse-item">324</div><div className="whse-count">{Whse_324}</div></div>
+        <div className="whse-item__container"><div className="whse-item">326</div><div className="whse-count">{Whse_326}</div></div>
+        <div className="whse-item__container"><div className="whse-item">329</div><div className="whse-count">{Whse_329}</div></div>
+        <div className="whse-item__container"><div className="whse-item">729</div><div className="whse-count">{Whse_729}</div></div>
+        <div className="whse-item__container"><div className="whse-item">739</div><div className="whse-count">{Whse_739}</div></div>
+        <div className="whse-item__container"><div className="whse-item">749</div><div className="whse-count">{Whse_749}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1025</div><div className="whse-count">{Whse_1025}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1062</div><div className="whse-count">{Whse_1062}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1070</div><div className="whse-count">{Whse_1070}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1081</div><div className="whse-count">{Whse_1081}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1093</div><div className="whse-count">{Whse_1093}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1103</div><div className="whse-count">{Whse_1103}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1114</div><div className="whse-count">{Whse_1114}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1166</div><div className="whse-count">{Whse_1166}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1174</div><div className="whse-count">{Whse_1174}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1177</div><div className="whse-count">{Whse_1177}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1199</div><div className="whse-count">{Whse_1199}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1211</div><div className="whse-count">{Whse_1211}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1214</div><div className="whse-count">{Whse_1214}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1215</div><div className="whse-count">{Whse_1215}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1236</div><div className="whse-count">{Whse_1236}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1334</div><div className="whse-count">{Whse_1334}</div></div>
+        <div className="whse-item__container"><div className="whse-item">1415</div><div className="whse-count">{Whse_1415}</div></div>
+        </div>
+        </React.Fragment>
+        )
+
+}
+
+
+export default WhseCount
--- src/views/dispatch/DriverDispatch.js
+++ src/views/dispatch/DriverDispatch.js
@@ -1,18 +1,16 @@
 import React, { lazy } from 'react'
 import './DriverDispatch.css'
-const CreateStore = lazy(() => import('./stores/createstore.js'))
+const WhseCount = lazy(() => import('../../../src/utils/WhseCount'))
 const CreateDriver = lazy(() => import('./drivers/createdriver.js'))
-const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
 
 
 const DriverDispatch = () => {
   return (
     <React.Fragment>
     <div className="main-container">
-    <WidgetsDropdown />
 
       <div className="store-div">
-        <CreateStore/>
+        <WhseCount/>
         </div>
         <div className="driver-div">
         <CreateDriver/>

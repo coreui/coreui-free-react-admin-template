e998bba328375ad0dd35361ac0db2bb605171a0f
Eric Velez <d175sfy@costco.com>
2020-09-12 11:36:51 -0400

    added TheHeader to DriverDispatch

--- src/views/dispatch/DriverDispatch.js
+++ src/views/dispatch/DriverDispatch.js
@@ -1,5 +1,6 @@
 import React, { lazy } from 'react'
 import './DriverDispatch.css'
+const TheHeader = lazy(() => import('../../../src/containers/TheHeader'))
 const WhseLoadCount = lazy(() => import('../../utils/loads/whseLoadCount/WhseLoadCount.js'))
 const CreateDriver = lazy(() => import('./drivers/createdriver.js'))
 const CreateDryModal = lazy(() => import('./modal/CreateDryModal'))
@@ -8,16 +9,18 @@ const CreateWetModal = lazy(() => import('./modal/CreateWetModal'))
 
 const DriverDispatch = () => {
   return (
-    <React.Fragment>
-    <div className="main-container">
 
-      <div className="store-div">
-        <WhseLoadCount/>
-        <CreateDryModal/>
-        <CreateWetModal/>
+    <React.Fragment>
+      <TheHeader />
+      <div className="main-container">
+
+        <div className="store-div">
+          <WhseLoadCount />
+          <CreateDryModal />
+          <CreateWetModal />
         </div>
         <div className="driver-div">
-        <CreateDriver/>
+          <CreateDriver />
         </div>
       </div>
     </React.Fragment>

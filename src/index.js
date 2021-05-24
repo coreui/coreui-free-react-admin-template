632c849fa71e81000cb0472da2e34efffff68a78
Łukasz Holeczek <lukasz@holeczek.pl>
2021-05-24 17:17:14 +0200

    refactor: remove ie11 polyfills

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'react-app-polyfill/ie11' // For IE 11 support
+// import 'react-app-polyfill/ie11' // For IE 11 support TODO: remove in final version
 import 'react-app-polyfill/stable'
 import 'core-js'
 import './polyfill'

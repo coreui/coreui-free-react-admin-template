3a96c33d167684a1ca5eb1759f920f63b43e7f5c
Łukasz Holeczek <lukasz@holeczek.pl>
2021-06-01 19:57:56 +0200

    style: improve syntax

--- src/components/index.js
+++ src/components/index.js
@@ -3,15 +3,6 @@ import AppContent from './AppContent'
 import AppFooter from './AppFooter'
 import AppHeader from './AppHeader'
 import AppHeaderDropdown from './header/AppHeaderDropdown'
-import DefaultLayout from '../layout/DefaultLayout'
 import AppSidebar from './AppSidebar'
 
-export {
-  AppBreadcrumb,
-  AppContent,
-  AppFooter,
-  AppHeader,
-  AppHeaderDropdown,
-  DefaultLayout,
-  AppSidebar,
-}
+export { AppBreadcrumb, AppContent, AppFooter, AppHeader, AppHeaderDropdown, AppSidebar }

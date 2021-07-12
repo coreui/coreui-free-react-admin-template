0eeafbdb5f01ea511725086b552737815caded4f
VW <71827661+vwvwMM@users.noreply.github.com>
2021-07-12 17:18:01 +0800

    NTUEEPLUS-2. Delete un-used routes and create column page
    Co-authored-by: vwvwMM <vincentwu0628@gmail.com>

--- src/_nav.js
+++ src/_nav.js
@@ -7,21 +7,21 @@ const _nav = [
     _component: 'CNavItem',
     anchor: '_nav/anchor',
   },
-  // {
-  //   _component: 'CNavItem',
-  //   as: NavLink,
-  //   anchor: 'Dashboard',
-  //   to: '/dashboard',
-  //   icon: <CIcon name="cil-speedometer" customClassName="nav-icon" />,
-  //   badge: {
-  //     color: 'info',
-  //     text: 'NEW',
-  //   },
-  // },
-  // {
-  //   _component: 'CNavTitle',
-  //   anchor: 'Theme',
-  // },
+  {
+    _component: 'CNavItem',
+    as: NavLink,
+    anchor: 'Dashboard',
+    to: '/dashboard',
+    icon: <CIcon name="cil-speedometer" customClassName="nav-icon" />,
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    },
+  },
+  {
+    _component: 'CNavTitle',
+    anchor: 'Theme',
+  },
   // {
   //   _component: 'CNavItem',
   //   as: NavLink,
@@ -289,37 +289,37 @@ const _nav = [
   //   _component: 'CNavTitle',
   //   anchor: 'Extras',
   // },
-  // {
-  //   _component: 'CNavGroup',
-  //   anchor: 'Pages',
-  //   icon: <CIcon name="cil-star" customClassName="nav-icon" />,
-  //   items: [
-  //     {
-  //       _component: 'CNavItem',
-  //       as: NavLink,
-  //       anchor: 'Login',
-  //       to: '/login',
-  //     },
-  //     {
-  //       _component: 'CNavItem',
-  //       as: NavLink,
-  //       anchor: 'Register',
-  //       to: '/register',
-  //     },
-  //     {
-  //       _component: 'CNavItem',
-  //       as: NavLink,
-  //       anchor: 'Error 404',
-  //       to: '/404',
-  //     },
-  //     {
-  //       _component: 'CNavItem',
-  //       as: NavLink,
-  //       anchor: 'Error 500',
-  //       to: '/500',
-  //     },
-  //   ],
-  // },
+  {
+    _component: 'CNavGroup',
+    anchor: 'Pages',
+    icon: <CIcon name="cil-star" customClassName="nav-icon" />,
+    items: [
+      {
+        _component: 'CNavItem',
+        as: NavLink,
+        anchor: 'Login',
+        to: '/login',
+      },
+      {
+        _component: 'CNavItem',
+        as: NavLink,
+        anchor: 'Register',
+        to: '/register',
+      },
+      {
+        _component: 'CNavItem',
+        as: NavLink,
+        anchor: 'Error 404',
+        to: '/404',
+      },
+      {
+        _component: 'CNavItem',
+        as: NavLink,
+        anchor: 'Error 500',
+        to: '/500',
+      },
+    ],
+  },
 ]
 
 export default _nav

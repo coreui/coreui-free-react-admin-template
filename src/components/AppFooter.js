3cf2fcf6554f4b188448dbb805d99a708504908b
VW <71827661+vwvwMM@users.noreply.github.com>
2021-07-27 15:45:54 +0800

    NTUEEPLUS-36. footer (#24)
    * finish typo of footer
    
    * finish footer
    
    Co-authored-by: vwvwMM <vincentwu0628@gmail.com>

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,23 +1,49 @@
 import React from 'react'
-import { CFooter } from '@coreui/react'
+import { CCol, CFooter, CRow } from '@coreui/react'
+import eesa from '../assets/images/eesa-icon.png'
+import { cilAlignCenter } from '@coreui/icons'
 
 const AppFooter = () => {
   return (
-    <CFooter>
-      <div>src/components/AppFooter/CFooter</div>
-      {/* <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
-        </a>
-        <span className="ms-1">&copy; 2021 creativeLabs.</span>
+    <div style={{ backgroundColor: '#002958', color: 'white' }}>
+      <CRow
+        xs={10}
+        style={{
+          width: '100%',
+        }}
+        alignHorizontal="center"
+      >
+        <CCol
+          xs={9}
+          style={{
+            lineHeight: '1.9rem',
+            fontSize: '1rem',
+            marginLeft: '0.5rem',
+            marginTop: '1rem',
+          }}
+        >
+          國立臺灣大學電機工程學系 系學會 <br />
+          <div style={{ fontWeight: '600' }}>
+            National Taiwan University Electrical Engineering Department Student Association <br />{' '}
+            Email: ntueesa@gmail.com
+          </div>
+        </CCol>
+        <CCol
+          xs={2}
+          style={{
+            margin: '0.5rem 0',
+          }}
+        >
+          <img src={eesa} alt="eesa" className="img-fluid" />
+        </CCol>
+      </CRow>
+      <div
+        className="d-flex flex-column align-items-center text-center"
+        style={{ backgroundColor: '#001936', width: '100%' }}
+      >
+        Copyright © 2021 NTUEESA
       </div>
-      <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI for React
-        </a>
-      </div> */}
-    </CFooter>
+    </div>
   )
 }
 

7436c8b472ee76dfffd1fca24af0b6d9d9789f9a
noidname01 <55401762+noidname01@users.noreply.github.com>
2021-07-31 23:57:39 +0800

    NTUEEPLUS-25. Forget Functionality (#33)

--- src/views/pages/forget/Forget.js
+++ src/views/pages/forget/Forget.js
@@ -13,7 +13,24 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
+const ForgetFormTemplate = {
+  studentID: '',
+}
+
 const Forget = () => {
+  const [forgetForm, setForgetForm] = useState(ForgetFormTemplate)
+
+  const handleInputChange = (e) => {
+    setForgetForm({ ...forgetForm, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // connect with backend
+    // and send email to user
+    console.log(forgetForm) // for test
+  }
+
   return (
     <div className="min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -28,11 +45,15 @@ const Forget = () => {
                     <CInputGroupText>
                       <CIcon name="cil-education" />
                     </CInputGroupText>
-                    <CFormControl placeholder="Student ID" />
+                    <CFormControl
+                      placeholder="Student ID"
+                      name="studentID"
+                      onChange={handleInputChange}
+                    />
                   </CInputGroup>
                   <CRow className="justify-content-center mt-3">
                     <div className="d-flex justify-content-center">
-                      <CButton color="dark" block>
+                      <CButton color="dark" block onClick={handleSubmit}>
                         Send Link to Mail
                       </CButton>
                     </div>

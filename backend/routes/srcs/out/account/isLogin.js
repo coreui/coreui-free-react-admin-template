40c2b1e802af700c4eb535199fab23b2f551cf8f
featherchen <57944334+featherchen@users.noreply.github.com>
2021-08-19 15:35:49 +0800

    NTUEEPLUS-104. isLogin API return userimage (#63)
    * return userimage in isLogin
    
    * not using sesstion

--- backend/routes/srcs/out/account/isLogin.js
+++ backend/routes/srcs/out/account/isLogin.js
@@ -1,5 +1,5 @@
-const { ErrorHandler } = require("../../../error")
-
+const { ErrorHandler } = require('../../../error')
+const Visual = require('../../../Schemas/user_visual_new')
 /**
  * @api {post} /isLogin isLogin
  * @apiName IsLogin
@@ -7,11 +7,14 @@ const { ErrorHandler } = require("../../../error")
  * @apiDescription 檢查是否有登入
  *
  * @apiSuccess (201) {String} account 登入者學號
- * 
+ *
  * @apiError (403) {String} description "未登入"
  */
-module.exports = (req, res, next) => {
-    const session_account = req.session.loginAccount
-    if(session_account) return res.status(200).send({account:session_account})
-    else throw new ErrorHandler(403,'未登入')
+module.exports = async (req, res, next) => {
+  const session_account = req.session.loginAccount
+  if (session_account) {
+    const user = await Visual.findOne({ account: session_account })
+    if (!user) throw new ErrorHandler(404, 'profile不存在')
+    return res.status(200).send({ account: session_account, userimage: user.imgSrc })
+  } else throw new ErrorHandler(403, '未登入')
 }

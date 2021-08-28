c35a385f514b46e3718be6637b904760555e3f55
VW <71827661+vwvwMM@users.noreply.github.com>
2021-08-28 17:39:49 +0800

    NTUEEPLUS-126. Contact page (#83)
    * simplest contact page
    
    * contact us
    
    Co-authored-by: vwvwMM <vincentwu0628@gmail.com>

--- client/src/views/components/contact/Contact.js
+++ client/src/views/components/contact/Contact.js
@@ -1,35 +1,36 @@
 /* eslint-disable prettier/prettier */
 import { CCol, CContainer, CRow, CButton } from '@coreui/react'
 import React from 'react'
-import { Link } from 'react-router-dom'
-import left_img from '../../../assets/images/left_minister.png'
-import right_img from '../../../assets/images/right_minister.png'
 
 const Contact = () => {
   return (
     <CContainer className="text-center">
+      <h1>Contact Us</h1>
       <CRow className="justify-content-evenly">
         <CCol xs={3}>
-          <img src={left_img} alt="部長 留學組" className="img-fluid mb-4" />
-          <h2 className="mb-3">李筠婕</h2>
-          <h3>B06901014</h3>
+          <i className="bi bi-instagram"></i>&emsp;<a href="https://www.instagram.com/ntueeplus/">IG</a>
         </CCol>
         <CCol xs={3}>
-          <img src={right_img} alt="部長 網頁組" className="img-fluid mb-4" />
-          <h2 className="mb-3">鄭謹譯</h2>
-          <h3>B06901180</h3>
+          <h5>QRCode</h5>
         </CCol>
       </CRow>
-      <CRow className="justify-content-center">
-        <Link to="/team">
-          <CButton
-            className="px-3 py-2"
-            style={{ fontWeight: 'bold', border: '0.2rem solid', borderRadius: '1rem' }}
-          >
-            Our Team
-          </CButton>
-        </Link>
+      <CRow className="justify-content-evenly">
+        <CCol xs={3}>
+          <i className="bi bi-facebook"></i>&emsp;<a href="https://www.facebook.com/groups/ntueeplus">FB</a>
+        </CCol>
+        <CCol xs={3}>
+          <h5>QRCode</h5>
+        </CCol>
       </CRow>
+      <CRow className="justify-content-evenly">
+        <CCol xs={3}>
+          <i className="bi bi-github"></i>&emsp;<a href="https://github.com/NTUEE-PLUS/EndOfWeb">Github</a>
+        </CCol>
+        <CCol xs={3}>
+          <h5>QRCode</h5>
+        </CCol>
+      </CRow>
+      
     </CContainer>
   )
 }

e440c628c47a8dfcd9cc72ef21b3c1df81cc86cb
noidname01 <55401762+noidname01@users.noreply.github.com>
2021-09-12 17:49:41 +0800

    NTUEEPLUS-118. Search Empty Appearance (#104)

--- client/src/views/in/profile/search/SearchProfile.js
+++ client/src/views/in/profile/search/SearchProfile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectSearch } from '../../../../slices/searchSlice'
 import default_male from '../../../../assets/images/default_male.png'
-import { CAvatar, CCallout, CContainer } from '@coreui/react'
+import { CAvatar, CCallout, CContainer, CImage } from '@coreui/react'
 import { makeStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -11,6 +11,7 @@ import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import { Link } from 'react-router-dom'
+import { no_result } from '.'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,7 +38,11 @@ const SearchProfile = () => {
   const classes = useStyles()
   const { resultProfiles } = useSelector(selectSearch)
   return resultProfiles.length === 0 ? (
-    <></>
+    <>
+      <CContainer className="align-items-center">
+        <CImage src={no_result} fluid />
+      </CContainer>
+    </>
   ) : (
     <CContainer className="align-items-center col-md-10 col-xl-8">
       <List className={classes.root}>
--- client/src/views/in/profile/search/index.js
+++ client/src/views/in/profile/search/index.js
@@ -1,2 +1,4 @@
 import SearchProfile from './SearchProfile'
+import no_result from '../../../../assets/images/no_result.png'
 export default SearchProfile
+export { no_result }

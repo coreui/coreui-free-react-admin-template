f376bb705b7fc420a2b371a1f3d5c0d589aba7e4
VW <71827661+vwvwMM@users.noreply.github.com>
2021-09-15 13:51:45 +0800

    NTUEEPLUS-150. Out interviews (#114)
    * create three interviews
    
    * add actual words in interviews
    
    * finish outline words
    
    * finish hover padding
    
    * redirect to login

--- client/src/views/out/home/header/Header.js
+++ client/src/views/out/home/header/Header.js
@@ -4,8 +4,8 @@ import { CContainer, CRow } from '@coreui/react'
 
 const Header = () => {
   return (
-    <header id="header">
-      <div className="intro" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+    <header id="header" data-aos-anchor-placement="bottom-bottom">
+      <div className="intro" data-aos="fade-up">
         <div className="overlay">
           <CContainer>
             <CRow className="justify-content-center">
--- client/src/views/out/home/interviews/Interviews.js
+++ client/src/views/out/home/interviews/Interviews.js
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import { CContainer } from '@coreui/react'
+import columnOutline1999 from '../../../../assets/images/1999_column_outline.jpg'
+import columnOutline2008 from '../../../../assets/images/2008_column_outline.jpg'
+import columnOutline2012 from '../../../../assets/images/2012_column_outline.jpg'
 
 const Interviews = () => {
   return (
@@ -8,66 +11,64 @@ const Interviews = () => {
       <CContainer data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className="section-title">
           <h2>Interviews</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+          <p>底下是我們節錄的一些採訪介紹，一起看看吧！</p>
         </div>
         <div className="d-flex flex-row">
-          <div className="col-xs-6 col-sm-4">
+          <div className="col-xs-12 col-sm-4">
             <div className="portfolio-item">
               <div className="hover-bg">
                 {' '}
-                <a
-                  href="https://picsum.photos/350/200"
-                  title="Project Title"
-                  data-lightbox-gallery="gallery1"
-                >
+                <a href="/#/login" title="Project Title" data-lightbox-gallery="gallery1">
                   <div className="hover-text">
-                    <h4>Lorem Ipsum</h4>
+                    <h3>2012級 李昀樵 </h3>
+                    <h4>17直播 技術副總</h4>
+                    <p className="mx-3">
+                      李昀樵曾是李琳山教授語音實驗室的研究生，研究所時期除了研究外，更有進行小型創業，開發出新聞摘要軟件及搜尋公共腳踏車的APP，前者以兩萬元售出，後者成為該領域當時市占率最高的APP。
+                    </p>
                   </div>
                   <img
-                    src="https://picsum.photos/350/200"
-                    className="img-responsive"
+                    src={columnOutline2012}
+                    className="img-fluid p-2 rounded"
                     alt="Project Title"
                   />{' '}
                 </a>{' '}
               </div>
             </div>
           </div>
-          <div className="col-xs-6 col-sm-4">
+          <div className="col-xs-12 col-sm-4">
             <div className="portfolio-item">
               <div className="hover-bg">
                 {' '}
-                <a
-                  href="https://picsum.photos/350/200"
-                  title="Project Title"
-                  data-lightbox-gallery="gallery1"
-                >
+                <a href="/#/login" title="Project Title" data-lightbox-gallery="gallery1">
                   <div className="hover-text">
-                    <h4>Adipiscing Elit</h4>
+                    <h3>1999級 簡韶逸</h3>
+                    <h4> (CEO/ Founder @ Ganzin Technology, Prof. @ NTUEE)</h4>
+                    <p className="mx-3">
+                      簡韶逸教授任職於台大電子所長達16年，致力於多媒體訊號處理系統、多媒體積體電路設計、晶片系統設計方法的研究。
+                      多年來，「媒體晶片系統實驗室」不斷研發出優異的技術。
+                    </p>
                   </div>
-                  <img
-                    src="https://picsum.photos/350/200"
-                    className="img-responsive"
-                    alt="Project Title"
-                  />{' '}
+                  <img src={columnOutline1999} className="img-fluid rounded" alt="Project Title" />{' '}
                 </a>{' '}
               </div>
             </div>
           </div>
-          <div className="col-xs-6 col-sm-4">
+          <div className="col-xs-12 col-sm-4">
             <div className="portfolio-item">
               <div className="hover-bg">
                 {' '}
-                <a
-                  href="https://picsum.photos/350/200"
-                  title="Project Title"
-                  data-lightbox-gallery="gallery1"
-                >
+                <a href="/#/login" title="Project Title" data-lightbox-gallery="gallery1">
                   <div className="hover-text">
-                    <h4>Lorem Ipsum</h4>
+                    <h3>2008級 鄭恆之</h3>
+                    <h4> (Technical Lead Manager @ Google Brain)</h4>
+                    <p className="mx-3">
+                      目前任職於 Google Brain
+                      的團隊技術領導者和軟體主管工程師的鄭恆之，從事大規模機器學習的研究與軟體開發。
+                    </p>
                   </div>
                   <img
-                    src="https://picsum.photos/350/200"
-                    className="img-responsive"
+                    src={columnOutline2008}
+                    className="img-fluid p-2 rounded"
                     alt="Project Title"
                   />{' '}
                 </a>{' '}

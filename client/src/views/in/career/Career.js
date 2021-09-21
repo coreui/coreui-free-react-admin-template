a8fa53f42e6cb8d536c10d0e19cbfcf573899b16
VW <71827661+vwvwMM@users.noreply.github.com>
2021-09-21 23:19:29 +0800

    NTUEEPLUS-168. Career Entry RWD (#130)
    * finish task
    
    * english to chinese
    
    * center career entry

--- client/src/views/in/career/Career.js
+++ client/src/views/in/career/Career.js
@@ -2,57 +2,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { CCol, CRow, CContainer, CImage } from '@coreui/react'
-import {Recruitment_image,Recommendation_image} from './index'
+import { Recruitment_image, Recommendation_image } from './index'
 
 const Career = () => {
   return (
-    <div className="d-flex flex-row align-items-center career">
+    <div className="d-flex flex-row align-items-center career min-vh-100">
       <CContainer className="align-items-center">
-        {/* for desktop and ipad */}
-        <CRow className="justify-content-around d-sm-none d-lg-flex">
-          <CCol xs="4">
+        <CRow className="justify-content-around d-flex">
+          <CCol md="4" sm="8">
             <Link className="d-flex justify-content-center" to="/recruitment">
               <CImage
-                fluid
                 src={Recruitment_image}
                 alt="Register by Account"
-                className="career_img"
+                className="career_img img-fluid"
               />
             </Link>
+            <h3 className="text-light text-center mt-4">來看看各家公司的徵才貼文！</h3>
           </CCol>
-          <CCol xs="4">
+          <CCol md="4" sm="8">
             <Link className="d-flex justify-content-center" to="/recommendation">
               <CImage
-                fluid
                 src={Recommendation_image}
                 alt="Register by Account"
-                className="career_img  "
+                className="career_img img-fluid"
               />
             </Link>
+            <h3 className="text-light text-center mt-4">電機系人才都在這！</h3>
           </CCol>
-        </CRow>
-        {/* for mobile */}
-        <CRow className="justify-content-center d-sm-flex d-lg-none">
-          <CRow className="justify-content-center mb-3">
-            <Link className="d-flex justify-content-center" to="/recruitment">
-              <CImage
-                fluid
-                src={Recruitment_image}
-                alt="Register by Account"
-                className="career_img  "
-              />
-            </Link>
-          </CRow>
-          <CRow className="justify-content-center mt-3">
-            <Link className="d-flex justify-content-center" to="/recommendation">
-              <CImage
-                fluid
-                src={Recommendation_image}
-                alt="Register by Account"
-                className="career_img  "
-              />
-            </Link>
-          </CRow>
         </CRow>
       </CContainer>
     </div>

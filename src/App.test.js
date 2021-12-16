1c3c27bc8d098fed7dce3c38148ef7a906b6f324
Łukasz Holeczek <lukasz@holeczek.pl>
2021-12-16 18:16:12 +0100

    refactor: migrate to Create React App v5

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { shallow } from 'enzyme/build'
+import { render, screen } from '@testing-library/react'
 import App from './App'
-import Dashboard from './views/dashboard/Dashboard.js'
 
-it('mounts App without crashing', () => {
-  const wrapper = shallow(<App />)
-  wrapper.unmount()
-})
-
-it('mounts Dashboard without crashing', () => {
-  const wrapper = shallow(<Dashboard />)
-  wrapper.unmount()
+test('renders learn react link', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,15 +1,5 @@
-import { configure } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-
-configure({ adapter: new Adapter() })
-
-if (global.document) {
-  document.createRange = () => ({
-    setStart: () => {},
-    setEnd: () => {},
-    commonAncestorContainer: {
-      nodeName: 'BODY',
-      ownerDocument: document,
-    },
-  })
-}
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';

ddc688744fcb61e68b45fbabd9d635d4ea202866
Łukasz Holeczek <lukasz@holeczek.pl>
2022-01-05 01:19:02 +0100

    refactor(CFooter): update copyrights

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -8,12 +8,12 @@ const AppFooter = () => {
         <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
           CoreUI
         </a>
-        <span className="ms-1">&copy; 2021 creativeLabs.</span>
+        <span className="ms-1">&copy; 2022 creativeLabs.</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
         <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI for React
+          CoreUI React Admin &amp; Dashboard Template
         </a>
       </div>
     </CFooter>

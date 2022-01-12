c628d5725e5a4ac299b16107c5c8562db0697798
connor226 <73697675+connor226@users.noreply.github.com>
2022-01-12 23:58:39 +0800

    NTUEEPLUS-197. modify matching api for db data (#167)
    * change CIcon name to icon
    fix conflict ==
    
    * fix CIcon bugs
    
    * fix conflict from 156
    
    * modify matching algorithm for db data
    
    * insert sample data
    
    * delete redundent comments

--- backend/routes/srcs/in/study/runMatch/main.js
+++ backend/routes/srcs/in/study/runMatch/main.js
@@ -15,14 +15,16 @@ const matching = require('./matching')
  * 
  * @apiSuccess (200) {File} - output.xlsx
  */
-const post = asyncHandle(async (req,res,next)=>{
+const post = asyncHandle(async (req, res, next) => {
+  /*
     const senior = req.files['senior'][0]
     const junior = req.files['junior'][0]
     console.log(senior)
-    console.log(junior)
-    await matching(senior.path,junior.path,__dirname+'/uploads/output.xlsx')
-    // await matching(__dirname+'/uploads/senior.xlsx',__dirname+'/uploads/junior.xlsx',__dirname+'/uploads/output.xlsx')
-    res.download(__dirname+'/uploads/output.xlsx')
+    console.log(junior)*/
+  await matching(/*senior.path,junior.path,__dirname+'/uploads/output.xlsx'*/)
+  // await matching(__dirname+'/uploads/senior.xlsx',__dirname+'/uploads/junior.xlsx',__dirname+'/uploads/output.xlsx')
+  // res.download(__dirname+'/uploads/output.xlsx')
+  res.status(204).send('Finish Matching!')
 })
 
-module.exports = [parseExcel,post]
+module.exports = [parseExcel, post]

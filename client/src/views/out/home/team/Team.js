ada4a489a9856151229f32217d6e0d3ad9e8d518
Alanhsiu <77971798+Alanhsiu@users.noreply.github.com>
2022-02-08 17:46:42 +0800

    NTUEEPLUS-234. Team members photos. (#192)
    Co-authored-by: Alan Hsieh <b09901066@ntu.edu.tw>

--- client/src/views/out/home/team/Team.js
+++ client/src/views/out/home/team/Team.js
@@ -13,7 +13,14 @@ const Team = () => {
           <div className="col-sm-3 col-xs-6 team">
             <div className="thumbnail">
               {' '}
-              <img src="https://i.imgur.com/eOWLfEO.png" alt="..." className="team-img img-fluid" />
+              <img
+                src="https://i.imgur.com/eOWLfEO.png"
+                alt="..."
+                className="team-img img-fluid"
+                style={{
+                  borderRadius: '50%',
+                }}
+              />
               <div className="caption">
                 <h4>卓昱辰</h4>
                 <p>留學採訪負責人</p>
@@ -23,7 +30,14 @@ const Team = () => {
           <div className="col-sm-3 col-xs-6 team">
             <div className="thumbnail">
               {' '}
-              <img src="https://i.imgur.com/y6P3fTw.png" alt="..." className="team-img img-fluid" />
+              <img
+                src="https://i.imgur.com/y6P3fTw.png"
+                alt="..."
+                className="team-img img-fluid"
+                style={{
+                  borderRadius: '50%',
+                }}
+              />
               <div className="caption">
                 <h4>王友廷</h4>
                 <p>網頁維護負責人</p>
@@ -33,7 +47,14 @@ const Team = () => {
           <div className="col-sm-3 col-xs-6 team">
             <div className="thumbnail">
               {' '}
-              <img src="https://i.imgur.com/VorzAuV.png" alt="..." className="team-img img-fluid" />
+              <img
+                src="https://i.imgur.com/VorzAuV.png"
+                alt="..."
+                className="team-img img-fluid"
+                style={{
+                  borderRadius: '50%',
+                }}
+              />
               <div className="caption">
                 <h4>陳亮君</h4>
                 <p>網頁維護負責人</p>
@@ -43,7 +64,14 @@ const Team = () => {
           <div className="col-sm-3 col-xs-6 team">
             <div className="thumbnail">
               {' '}
-              <img src="https://i.imgur.com/5psCEQS.png" alt="..." className="team-img img-fluid" />
+              <img
+                src="https://i.imgur.com/5psCEQS.png"
+                alt="..."
+                className="team-img img-fluid"
+                style={{
+                  borderRadius: '50%',
+                }}
+              />
               <div className="caption">
                 <h4>巫竑儒</h4>
                 <p>網頁維護負責人</p>

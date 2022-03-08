c56acc85d8881a158c545ae18cc9410075614a95
Łukasz Holeczek <lukasz@holeczek.pl>
2022-03-08 02:17:16 +0100

    refactor: migrate to Create React App v5

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'core-js'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './store'
 
@@ -14,7 +14,7 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()

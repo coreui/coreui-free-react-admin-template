027c69a0cf2189b581f566f8de6c4dbf2264f37e
Łukasz Holeczek <lukasz@holeczek.pl>
2022-03-08 02:20:01 +0100

    build: update build tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   extends: [
     'react-app',
+    'react-app/jest',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],

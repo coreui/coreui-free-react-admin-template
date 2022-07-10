07d038ab6f21fcf76154d9c6d5c94b3b8dc3579f
Łukasz Holeczek <lukasz@holeczek.pl>
2022-07-10 15:15:37 +0200

    refactor: remove docs version from urls

--- src/components/DocsCallout.js
+++ src/components/DocsCallout.js
@@ -2,14 +2,12 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { CCallout, CLink } from '@coreui/react'
 
-import packageJson from '../../package.json'
-
 const DocsCallout = (props) => {
   const { content, href, name } = props
 
   const plural = name.slice(-1) === 's' ? true : false
 
-  const _href = `https://coreui.io/react/docs/${packageJson.config.coreui_library_short_version}/${href}`
+  const _href = `https://coreui.io/react/docs/${href}`
 
   return (
     <CCallout color="info" className="bg-white">

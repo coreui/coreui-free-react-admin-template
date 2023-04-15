faec25094533a4ab83f8ee970a8dfbdb81059763
triracle97 <triracle@gmail.com>
2023-04-15 21:41:09 +0700

    Update production url

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,1 @@
-export const BACKEND_HOST = process.env.NODE_ENV === 'development' ? 'http://51.79.147.198:3000' : '';
+export const BACKEND_HOST = process.env.NODE_ENV === 'development' ? 'http://51.79.147.198:3000' : 'http://localhost:3000';

0fac35e7fe4ba7f86b405baa4957695ee90605c3
Behnam Jaberi <behnam_it87@yahoo.com>
2023-07-03 17:56:07 +0330

    Add supported network

---
+++ src/components/header/auth/SupportedNetwork.js
@@ -0,0 +1,21 @@
+// supportedNetwork.js
+import ethIcon from '../../../assets/images/tokens/eth.png';
+import arbitIcon from '../../../assets/images/tokens/arbitrum.svg';
+import maticIcon from '../../../assets/images/tokens/polygon.svg';
+
+const supportedNetwork = [
+  {
+    name: 'Ethereum',
+    cover: ethIcon,
+  },
+  {
+    name: 'Arbitrum One',
+    cover: arbitIcon,
+  },
+  {
+    name: 'Polygon',
+    cover: maticIcon,
+  },
+];
+
+export default supportedNetwork;

ce581066d9104411aed466003a346cbf97bbaaaa
Behnam Jaberi <behnam_it87@yahoo.com>
2023-07-28 20:23:33 +0330

    Update sidebar acions from redux

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,34 +1,21 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-
+import { useSelector } from 'react-redux'
 import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-
 import { AppSidebarNav } from './AppSidebarNav'
-
 import { logoNegative } from 'src/assets/brand/logo-negative'
 import { sygnet } from 'src/assets/brand/sygnet'
-
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
-
-// sidebar nav config
 import navigation from '../_nav'
+import { useToggleSidebar } from '../redux/modules/init'
 
 const AppSidebar = () => {
-  const dispatch = useDispatch()
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+  const { sidebarShow, toggleSidebar } = useToggleSidebar()
 
   return (
-    <CSidebar
-      position="fixed"
-      unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
-      }}
-    >
+    <CSidebar position="fixed" unfoldable={unfoldable} visible={sidebarShow}>
       <CSidebarBrand className="d-none d-md-flex" to="/">
         <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
         <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
@@ -38,10 +25,7 @@ const AppSidebar = () => {
           <AppSidebarNav items={navigation} />
         </SimpleBar>
       </CSidebarNav>
-      <CSidebarToggler
-        className="d-none d-lg-flex"
-        onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
-      />
+      <CSidebarToggler className="d-none d-lg-flex" onClick={toggleSidebar} />
     </CSidebar>
   )
 }

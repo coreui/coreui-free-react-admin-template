cf5398357ddde507323b7d93ace81f53aeb48627
mrholek <lukasz@holeczek.pl>
2024-06-12 11:43:04 +0200

    chore: clean-up

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import autoprefixer from 'autoprefixer'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     base: './',
     build: {

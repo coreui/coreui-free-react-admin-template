e9c3185faf46212fe52f7997b15e09424676b492
Dmitry7007 <dimoz2002smirnov@gmail.com>
2024-10-09 21:04:17 +0700

    10.10

--- back/main.py
+++ back/main.py
@@ -140,3 +140,27 @@ async def reset_password(password_reset: schemas.UserResetPassword, db: Session 
         raise HTTPException(status_code=404, detail="User not found")
     crud.reset_password(db, user, password_reset.password)
     return {"message": "Password reset successfully"}
+
+@app.get("/incomes_by_month", response_model=schemas.IncomesByMonth)
+async def get_incomes_month(wallet_id: schemas.IncomesByMonth, month: schemas.IncomesByMonth, db: Session = Depends(get_db)):
+    return crud.incomes_by_wallet_id_and_month(db, wallet_id, month)
+
+@app.get("/expenses_by_month", response_model=schemas.ExpensesByMonth)
+async def get_expenses_month(wallet_id: schemas.ExpensesByMonth, month: schemas.ExpensesByMonth, db: Session = Depends(get_db)):
+    return crud.expenses_by_wallet_id_and_month(db, wallet_id, month)
+
+@app.get("/incomes_by_category", response_model=schemas.IncomesByCategory)
+async def get_incomes_category(wallet_id: schemas.IncomesByCategory, category: schemas.IncomesByCategory, db: Session = Depends(get_db)):
+    return crud.incomes_by_wallet_id_and_category(db, wallet_id, category)
+
+@app.get("/expenses_by_category", response_model=schemas.ExpensesByCategory)
+async def get_expenses_category(wallet_id: schemas.ExpensesByCategory, category: schemas.ExpensesByCategory, db: Session = Depends(get_db)):
+    return crud.expenses_by_wallet_id_and_category(db, wallet_id, category)
+
+@app.get("/incomes_by_year", response_model=schemas.IncomesByYear)
+async def get_incomes_year(wallet_id: schemas.IncomesByYear, year: schemas.IncomesByYear, db: Session = Depends(get_db)):
+    return crud.incomes_by_wallet_id_and_year(db, wallet_id, year)
+
+@app.get("/expenses_by_year", response_model=schemas.ExpensesByYear)
+async def get_expenses_year(wallet_id: schemas.ExpensesByYear, year: schemas.ExpensesByYear, db: Session = Depends(get_db)):
+    return crud.expenses_by_wallet_id_and_year(db, wallet_id, year)

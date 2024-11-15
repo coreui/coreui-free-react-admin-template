0b705457b7c7aa2bc3eaacd38361c3e53442a1e9
mrholek <lukasz@holeczek.pl>
2024-11-15 13:46:57 +0100

    build: update vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig(() => {
       preprocessorOptions: {
         scss: {
           quietDeps: true,
+          silenceDeprecations: ['import', 'legacy-js-api'],
         },
       },
     },

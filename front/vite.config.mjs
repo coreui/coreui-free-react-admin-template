489422412d67bd5167ef89b491e84b20876db4d9
Dmitry7007 <99890690+Dmitry7007@users.noreply.github.com>
2024-11-21 16:35:14 +0700

    Update vite.config.mjs

--- front/vite.config.mjs
+++ front/vite.config.mjs
@@ -46,7 +46,7 @@ export default defineConfig(() => {
     },
     server: {
       host: '0.0.0.0',
-      port: 3000,
+      port: 3001,
       proxy: {
         // https://vitejs.dev/config/server-options.html
       },

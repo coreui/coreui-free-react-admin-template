2218a64a009dafb46b99568ff7b6eea4473f07ef
mrholek <lukasz@holeczek.pl>
2025-02-04 13:11:37 +0100

    refactor: migrate ESLint to version 9 with a flat configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintPluginReact from 'eslint-plugin-react'
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
+
+export default [
+  { ignores: ['eslint.config.mjs'] },
+  {
+    ...eslintPluginReact.configs.flat.recommended,
+    ...eslintPluginReact.configs.flat['jsx-runtime'],
+    files: ['src/**/*.{js,jsx}'],
+    plugins: {
+      eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
+    },
+  },
+  eslintPluginPrettierRecommended,
+]

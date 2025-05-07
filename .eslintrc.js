baf77326653b9d698f60e5664af1d2d87a5f8b31
Mohamed Amine DEROUICH <medaminederouich@outlook.com>
2025-05-07 14:56:08 +0100

    feat: update ESLint configuration and dependencies for improved linting support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
+  env: {
+    browser: true, // Enables browser global variables
+    es2021: true, // Enables ES2021 global variables
+    node: true, // Enables Node.js global variables
+    jest: true, // Enables Jest global variables
+  },
+  extends: [
+    'eslint:recommended', // Uses the recommended rules from ESLint
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:react-hooks/recommended', // Enforces the Rules of Hooks
+  ],
   // parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
@@ -7,16 +19,12 @@ module.exports = {
       jsx: true, // Allows for the parsing of JSX
     },
   },
+  plugins: ['react', 'react-hooks'],
   settings: {
     react: {
       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
   },
-  extends: [
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
-  plugins: ['react', 'react-hooks'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",

f66465cdc84ce3dd08972ee85c0336140aa26434
Mohamed Amine DEROUICH <medaminederouich@outlook.com>
2025-05-21 17:22:43 +0100

    feat: wrap ToastContainer with StyledEngineProvider for improved styling integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import StoreProvider from './store'
 import App from './App'
 import { createRoot } from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
+import { StyledEngineProvider } from '@mui/material/styles'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
@@ -11,15 +12,17 @@ const root = createRoot(container)
 root.render(
   <React.StrictMode>
     <StoreProvider>
-      <ToastContainer
-        hideProgressBar
-        pauseOnFocusLoss={false}
-        autoClose={2000}
-        closeOnClick
-        position="top-center"
-        theme="colored"
-      />
-      <App />
+      <StyledEngineProvider injectFirst>
+        <ToastContainer
+          hideProgressBar
+          pauseOnFocusLoss={false}
+          autoClose={2000}
+          closeOnClick
+          position="top-center"
+          theme="colored"
+        />
+        <App />
+      </StyledEngineProvider>
     </StoreProvider>
   </React.StrictMode>,
 )

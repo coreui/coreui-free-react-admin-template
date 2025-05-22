20b87060f34b98df92349225535a3021a3df2cb1
Mohamed Amine DEROUICH <medaminederouich@outlook.com>
2025-05-22 11:37:08 +0100

    feat: remove unused Register, Page404, and Page500 components for a cleaner codebase

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 const App = () => {
   const dispatch = useDispatch()
@@ -57,9 +54,6 @@ const App = () => {
       >
         <Routes>
           <Route path="/login" name="Login Page" element={<Login />} />
-          <Route path="/register" name="Register Page" element={<Register />} />
-          <Route path="/404" name="Page 404" element={<Page404 />} />
-          <Route path="/500" name="Page 500" element={<Page500 />} />
           <Route element={<PrivateRoute />} exact>
             <Route path="/" element={<DefaultLayout />} />
             <Route path="*" element={<DefaultLayout />} />

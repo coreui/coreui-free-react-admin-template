a117cca3535effeb0f9efab50fe39e6467be2d8d
Mohamed Amine DEROUICH <medaminederouich@outlook.com>
2025-06-23 11:13:39 +0100

    fix: update footer links and copyright year in AppFooter component

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,15 +5,15 @@ const AppFooter = () => {
   return (
     <CFooter className="px-4">
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          PFE
         </a>
-        <span className="ms-1">&copy; 2024 creativeLabs.</span>
+        <span className="ms-1">&copy; 2025</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI React Admin &amp; Dashboard Template
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          Mohamed Amine DEROUICH
         </a>
       </div>
     </CFooter>
